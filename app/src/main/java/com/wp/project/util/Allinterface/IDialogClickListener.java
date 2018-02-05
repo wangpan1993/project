ba8018da3220b414ee60ac94e31a10636e9c3c3d
wangpan <>
2018-02-05 15:56:23 +0800

    添加了一个拨打电话的方法； 添加了一个统一提示的dialog； 添加了一个获取权限的方法；

---
+++ app/src/main/java/com/wp/project/util/Allinterface/IDialogClickListener.java
@@ -0,0 +1,13 @@
+package com.wp.project.util.Allinterface;
+
+/**
+ * Created by WangPan on 2018/2/5.
+ */
+
+public interface IDialogClickListener {
+
+    public void onConfirm();
+
+    public void onCancel();
+
+}
--- app/src/main/java/com/wp/project/util/AppUtils.java
+++ app/src/main/java/com/wp/project/util/AppUtils.java
@@ -1,10 +1,26 @@
 package com.wp.project.util;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.wp.project.R;
+import com.wp.project.util.Allinterface.IDialogClickListener;
 
 import java.security.MessageDigest;
 
@@ -98,5 +114,110 @@ public class AppUtils {
 
         return result;
 
+    }
+
+    public static boolean isAllPermissionGranted(Context mContext,
+                                                 String permissionList[]) {
+        if (permissionList.length == 0)
+            return false;
+        for (int i = 0; i < permissionList.length; i++) {
+            if (ContextCompat.checkSelfPermission(mContext, permissionList[i]) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 拨打电话
+     * @param mContext 猜猜这是啥
+     * @param phoneNum 打给谁
+     * @param msg 提示语
+     * @param requestCode 假如没有权限的话——————你说咋办就咋办
+     */
+    public static void callPhoneNum(final Activity mContext, final String phoneNum, String msg, int requestCode)
+    {
+        if(AppUtils.isAllPermissionGranted(mContext, new String[]{Manifest.permission.CALL_PHONE}))
+        {
+            AlertUtils.showConfirmDialog(mContext, msg,
+                    true, new IDialogClickListener() {
+                        @SuppressLint("MissingPermission")
+                        @Override
+                        public void onConfirm() {
+                            Intent intent = new Intent(Intent.ACTION_CALL, Uri
+                                    .parse("tel:" + phoneNum));
+                            mContext.startActivity(intent);
+                        }
+                        @Override
+                        public void onCancel() {
+                        }
+                    });
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(mContext, new String[]{Manifest.permission.CALL_PHONE}, requestCode);
+        }
+
+
+    }
+
+    //提示对话框
+    public static void showConfirmDialog(final Activity context, String message ,boolean cancelVisibleFlag,final IDialogClickListener iDialogClickListener) {
+        if(context!=null&&!context.isFinishing())
+        {
+            final Dialog dialog = new Dialog(context, R.style.CustomAlertDialog);
+            Window win = dialog.getWindow();
+
+            win.getDecorView().setPadding(AppUtils.getWithDisplay(context)/10, 0, AppUtils.getWithDisplay(context)/10, 0);
+            WindowManager.LayoutParams lp = win.getAttributes();
+            lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+            lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            win.setAttributes(lp);
+
+            View v = context.getLayoutInflater().inflate(
+                    R.layout.layout_message_dialog, null);
+
+            TextView tv_message = (TextView) v.findViewById(R.id.tv_message);
+            tv_message.setText(message);
+
+            Button bt_confirm = (Button) v.findViewById(R.id.bt_confirm);
+            bt_confirm.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                    if(iDialogClickListener!=null)
+                        iDialogClickListener.onConfirm();
+                }
+            });
+            Button bt_cacel = (Button) v.findViewById(R.id.bt_cacel);
+            if(cancelVisibleFlag)
+                bt_cacel.setVisibility(View.VISIBLE);
+            bt_cacel.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if(iDialogClickListener!=null)
+                        iDialogClickListener.onCancel();
+                    dialog.dismiss();
+                }
+            });
+            dialog.setContentView(v);
+            dialog.setCancelable(false);
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
+    }
+
+    /**
+     * 获取屏幕的宽度
+     *
+     * @param mContext
+     * @return
+     */
+    public static int getWithDisplay(Context mContext) {
+
+        return mContext.getResources().getDisplayMetrics().widthPixels;
     }
 }
